e476494f670eb77167b40b7d735bb386abd40b88
Leonardo Pais <leovinicius3211@gmail.com>
2024-10-27 18:13:03 -0300

    Agregação entre objetos em java

---
+++ 03.Agregacao-entre-objetos/tecnico.java
@@ -0,0 +1,17 @@
+package class5;
+
+public class Tecnico {
+	private String NmTecnico;
+	
+	public Tecnico(String NmTecnico){
+		this.NmTecnico=NmTecnico;
+	}
+
+	public String getNmTecnico() {
+		return NmTecnico;
+	}
+	public void setNmTecnico(String nmTecnico) {
+		NmTecnico = nmTecnico;
+	}
+	
+}
